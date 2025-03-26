4d14e255672ab4da03230802f8097bb50748483a
krusand <idawennergaard@gmail.com>
2025-03-26 11:56:32 +0100

    Added experiment and participant_id

--- src/extracting_samples.py
+++ src/extracting_samples.py
@@ -15,7 +15,7 @@ def convert_asc_to_pq():
         "x_velocity_left",
         "y_velocity_left",
         "x_velocity_right",
-        "y_velocity_rigth",
+        "y_velocity_right",
         "x_resolution",
         "y_resolution",
         ".",
@@ -26,18 +26,48 @@ def convert_asc_to_pq():
     for file_filter, experiment in zip(file_filters, experiments):
         asc_files = [f for f in os.listdir(ASC_RAW_SAMPLES_DIR) if f.endswith('.asc') and f.startswith(f"{file_filter}")]
 
-        samples_dfs = []
-        for file_path in tqdm(asc_files):
-            df = pd.read_csv(file_path, names=colnames, delimiter="\t", skipinitialspace=True)        
+        samples = []
+        for file_name in tqdm(asc_files):
+            file_path = ASC_RAW_SAMPLES_DIR / file_name
+            df = pd.read_csv(
+                            file_path, 
+                            names=colnames, 
+                            delimiter="\t", 
+                            skipinitialspace=True,
+                            na_values={col: "." for col in [  # Apply na_values ONLY to numeric columns
+                                "time", "x_left", "y_left", "pupil_size_left", "x_right", "y_right", 
+                                "pupil_size_right", "x_velocity_left", "y_velocity_left", 
+                                "x_velocity_right", "y_velocity_right", "x_resolution", "y_resolution"
+                            ]},
+                            dtype = {"time": int,
+                                      "x_left": float,
+                                      "y_left": float,
+                                      "pupil_size_left": float,
+                                      "x_right": float,
+                                      "y_right": float,
+                                      "pupil_size_right": float,
+                                      "x_velocity_left": float,
+                                      "y_velocity_left": float,
+                                      "x_velocity_right": float,
+                                      "y_velocity_right": float,
+                                      "x_resolution": float,
+                                      "y_resolution": float,
+                                      ".": str,
+                                      "error_message": str})
+            df = df.drop(".", axis=1)        
             for col in df.columns:
                 if col not in [".", "error_message"]:
                     df[col] = pd.to_numeric(df[col], errors='coerce')
             
-            samples_dfs.append(df)
+            participant_id = file_name.split("_")[1]
+            df.insert(loc=0, column='participant_id', value=participant_id)
+            df.insert(loc=0, column='experiment', value=experiment)
+            samples.append(df)
         
         path_save = PROCESSED_DIR / f"{experiment}_SAMPLES.pq"
         print(f"Saving to {path_save}")
-        samples_dfs.to_parquet(path_save, index=False)
+        samples_df = pd.concat(samples)
+        samples_df.to_parquet(path_save, index=False)
 
 def main():
     # Convert asc files to parquet files

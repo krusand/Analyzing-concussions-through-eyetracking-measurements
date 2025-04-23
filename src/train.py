6348a0e09b9899b82065f51ff98eed27b7c12fa4
krusand <krusand@live.dk>
2025-04-23 09:19:23 +0200

    train on significant features

--- src/train.py
+++ src/train.py
@@ -6,15 +6,41 @@ import pandas as pd
 from config import *
 features = pd.read_parquet(FEATURES_DIR / "features.pq")
 
-y_data = features["y"]
-X_data = features.drop(["participant_id", "y"], axis=1)
+sig_features = ['ANTI_SACCADE_total_acceleration_magnitude_right_max',
+ 'ANTI_SACCADE_total_acceleration_magnitude_right_median',
+ 'FITTS_LAW_avg_fixations_pr_second',
+ 'FITTS_LAW_std_fixations_pr_second',
+ 'FITTS_LAW_mean_amplitude_sacc',
+ 'FITTS_LAW_mean_duration_sacc',
+ 'FITTS_LAW_mean_duration_fix',
+ 'FITTS_LAW_total_acceleration_magnitude_right_mean',
+ 'FITTS_LAW_total_acceleration_magnitude_right_median',
+ 'REACTION_reaction_time_avg',
+ 'REACTION_reaction_time_std',
+ 'REACTION_total_acceleration_magnitude_right_median',
+ 'EVIL_BASTARD_mean_duration_sacc',
+ 'EVIL_BASTARD_total_acceleration_magnitude_right_max',
+ 'EVIL_BASTARD_total_acceleration_magnitude_right_median',
+ 'EVIL_BASTARD_distance_to_fixpoint_max',
+ 'SHAPES_total_acceleration_magnitude_right_mean',
+ 'SHAPES_total_acceleration_magnitude_right_median',
+ 'SMOOTH_PURSUITS_mean_peak_velocity_sacc',
+ 'SMOOTH_PURSUITS_mean_duration_sacc',
+ 'SMOOTH_PURSUITS_total_acceleration_magnitude_right_max',
+ 'SMOOTH_PURSUITS_total_acceleration_magnitude_right_median',
+ 'SMOOTH_PURSUITS_Var_total',
+ 'SMOOTH_PURSUITS_distance_to_fixpoint_max',
+ 'SMOOTH_PURSUITS_distance_to_fixpoint_std']
 
+y_data = features["y"]
+# X_data = features.drop(["participant_id", "y"], axis=1)
+X_data = features[sig_features]
 
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=.2)
 
 pipe = Pipeline([
     ("scaler", StandardScaler()),
-    ("clf", RandomForestClassifier(max_depth=2))
+    ("clf", RandomForestClassifier(max_depth=4))
 ])
 
 

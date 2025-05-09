a825ae0f8b08c5f768618c7a4ba8d1f635114c73
krusand <idawennergaard@gmail.com>
2025-05-09 12:07:47 +0200

    Working...

--- src/clustering_utils.py
+++ src/clustering_utils.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.preprocessing import Normalizer
 
+# import umap
+
 from config import *
 
 #########################
@@ -106,3 +108,59 @@ def remove_multicollineraity(X, y, feature_corr_threshold=0.85,verbose=True):
     logging.info(f"Removed {len(to_drop)} highly correlated features: {to_drop}")
     
     return X_reduced
+
+def remove_outliers(X, method:str = "MAD", threshold:float = 3.0):
+    if method == 'MAD':
+        for col in X.columns:
+            data = X[col]
+            median = np.nanmedian(data)
+            abs_diff = np.abs(data - median)
+            mad = np.nanmedian(abs_diff)
+
+            if mad == 0 or np.isnan(mad):
+                continue
+
+            outlier_mask = (np.abs(data - median)) > (threshold * mad)
+            X.loc[outlier_mask, col] = np.nan
+        return X
+    else:
+        return X
+
+
+################
+###   Plot   ###
+################
+
+ 
+def plot_scatter_dimensionality_reduction(X, reducer=None, labels=None , arrows:bool=True, ax=None):
+    coeff = None
+    n_features_provided = X.shape[1]
+    if reducer is not None and n_features_provided > 2:
+        X_reduced = reducer.fit_transform(X)
+        if "PCA" in reducer.__str__():
+            coeff = reducer.components_
+        X_reduced_df = pd.DataFrame({"Component 1": X_reduced[:,0]
+                                ,'Component 2': X_reduced[:,1]})
+    else:
+        if n_features_provided != 2:
+            raise ValueError('Dimensionality reduction method must be provided when number of features are not 2')
+        X_reduced_df = X
+
+    x_var_name, y_var_name = X_reduced_df.columns
+
+    sns.scatterplot(X_reduced_df, x=x_var_name, y=y_var_name,ax=ax)
+    if "umap" in reducer.__str__().lower():
+        title_reducer = "UMAP()"
+    else:
+        title_reducer = reducer.__str__().upper()
+    ax.set_title(f"Method: {title_reducer}")
+    if coeff is not None and arrows:
+        n = coeff.shape[1]
+        for i in range(n):
+            plt.arrow(0, 0, coeff[0,i]*5, coeff[1,i]*5,color = 'r',alpha = 0.5)
+            if labels is None:
+                plt.text(coeff[0,i]* 5, coeff[1,i] * 5, "Var"+str(i+1), color = 'g', ha = 'center', va = 'center')
+            else:
+                print(labels[i])
+                plt.text(coeff[0,i]* 5, coeff[1,i] * 5, labels[i], color = 'g', ha = 'center', va = 'center')
+       
